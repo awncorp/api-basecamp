d920b39f123e3bd8053039cff2ff027106d60d1e
Al Newkirk <anewkirk@ana.io>
2015-09-28 13:10:04 +0000

    Updated inconsistency in documentation.

--- lib/API/Basecamp.pm
+++ lib/API/Basecamp.pm
@@ -176,7 +176,7 @@ The debug attribute if true prints HTTP requests and responses to standard out.
     $basecamp->fatal(1);
 
 The fatal attribute if true promotes 4xx and 5xx server response codes to
-exceptions, a L<API::Basecamp::Exception> object.
+exceptions, a L<API::Client::Exception> object.
 
 =cut
 
